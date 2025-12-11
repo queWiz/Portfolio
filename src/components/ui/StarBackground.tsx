23699f9462e438d0171c160dd165b07e07d8239d
Uwais Alqarni <ualqarni70@gmail.com>
2025-12-11 12:55:13 +0800

    chore: vercel deployment fix

--- src/components/ui/StarBackground.tsx
+++ src/components/ui/StarBackground.tsx
@@ -6,7 +6,7 @@ import { Points, PointMaterial } from "@react-three/drei";
 import * as random from "maath/random/dist/maath-random.esm";
 
 const StarBackground = (props: any) => {
-  const ref = useRef<any>();
+  const ref = useRef<any>(null);
   // Generate 5000 stars in a sphere
   const [sphere] = useState(() =>
     random.inSphere(new Float32Array(5000), { radius: 1.2 })
